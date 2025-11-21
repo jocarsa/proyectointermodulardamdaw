71f46c2972a58cfc48833462d09361120dace31c
Jose Vicente Carratala <info@josevicentecarratala.com>
2025-11-21 10:38:10 +0100

    Create 008-paso tabla bidimensional.py

---
+++ 102-Ejercicios prácticos segundo trimestre/103-Jinja/008-paso tabla bidimensional.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template 
+import mysql.connector                    # Importamos MySQL
+###################### MYSQL ##############################################
+conexion = mysql.connector.connect(
+host="localhost",user="portafolioceac",password="portafolioceac",database="portafolioceac"
+)                                         # Datos de conexión a la base de datos
+cursor = conexion.cursor()                # Creo un cursor MySQL
+# ----------------- ESTO ENVIA LAS TABLAS----------------------------------
+cursor.execute("SHOW TABLES;")            # Muestra las tablas de la base de datos
+tablas = []                               # Creo una lista vacia
+filas = cursor.fetchall()                 # Lo guardo en una lista
+for fila in filas:                        # Recorro el resultado
+  tablas.append(fila[0])                  # Lo añado a la lista de tablas  
+# ----------------- ESTO ENVIA LAS CABECERAS DE LAS TABLAS ---------
+cursor.execute("SHOW COLUMNS in Piezas;") # Muestra las tablas de la base de datos
+columnas = []                             # Creo una lista vacia
+filas = cursor.fetchall()                 # Lo guardo en una lista
+for fila in filas:                        # Recorro el resultado
+  columnas.append(fila[0])                # Lo añado a la lista de tablas 
+# ----------------- ESTO ENVIA TODA LA TABLA ---------
+cursor.execute("SELECT * FROM Piezas;")   # Muestra las tablas de la base de datos
+contenido_tabla = cursor.fetchall()       # Lo guardo en una lista
+###################### MYSQL ##############################################
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+  return render_template(
+      "backoffice.html",
+      mis_tablas = tablas,
+      mis_columnas = columnas,
+      mi_contenido_tabla = contenido_tabla
+  ) # Envio las tablas a HTML
+  
+if __name__ == "__main__":
+  app.run(debug=True)
+  
+
